86bc8080b5fa942a4305e50bdf013b7a0c7c7ce1
李铭昕 <715557344@qq.com>
2021-08-04 23:05:53 +0800

    增加嵌套事务使用示例

--- app/Controller/UserController.php
+++ app/Controller/UserController.php
@@ -11,6 +11,8 @@ declare(strict_types=1);
  */
 namespace App\Controller;
 
+use Hyperf\DbConnection\Annotation\Transactional;
+use Hyperf\DbConnection\Db;
 use Hyperf\HttpServer\Annotation\AutoController;
 use Hyperf\HttpServer\Contract\RequestInterface;
 use Hyperf\HttpServer\Contract\ResponseInterface;
@@ -21,5 +23,38 @@ class UserController extends Controller
     public function index(RequestInterface $request, ResponseInterface $response)
     {
         return $response->raw('Hello Hyperf!');
+    }
+
+    public function trans()
+    {
+        Db::transaction(function () {
+            // Do something...
+            Db::transaction(function () {
+                // Do something...
+            });
+            // Do something...
+        });
+
+        return $this->response->success('Hello Hyperf!');
+    }
+
+    public function trans2()
+    {
+        $this->foo();
+        return $this->response->success('Hello Hyperf!');
+    }
+
+    #[Transactional]
+    protected function foo()
+    {
+        // Do something...
+        $this->bar();
+        // Do something...
+    }
+
+    #[Transactional]
+    protected function bar()
+    {
+        // Do something...
     }
 }
