6621fa2261431608b375b56e29f9505d37ee0ca5
李铭昕 <715557344@qq.com>
2022-06-23 11:18:48 +0800

    Update

--- app/Controller/WebSocketController.php
+++ app/Controller/WebSocketController.php
@@ -14,23 +14,27 @@ namespace App\Controller;
 use Hyperf\Contract\OnCloseInterface;
 use Hyperf\Contract\OnMessageInterface;
 use Hyperf\Contract\OnOpenInterface;
+use Hyperf\Di\Annotation\Inject;
 use Hyperf\WebSocketServer\Context;
-use Swoole\Http\Request;
-use Swoole\WebSocket\Frame;
+use Hyperf\WebSocketServer\Sender;
 
 class WebSocketController implements OnMessageInterface, OnCloseInterface, OnOpenInterface
 {
+    #[Inject]
+    protected Sender $sender;
+
     public function onClose($server, int $fd, int $reactorId): void
     {
         var_dump('closed');
     }
 
-    public function onMessage($server, Frame $frame): void
+    public function onMessage($server, $frame): void
     {
         var_dump(Context::get('username'));
+        $this->sender->push($frame->fd, 'Received: ' . $frame->data);
     }
 
-    public function onOpen($server, Request $request): void
+    public function onOpen($server, $request): void
     {
         Context::set('username', 'test');
         var_dump('open');
--- config/routes.php
+++ config/routes.php
@@ -13,6 +13,6 @@ use Hyperf\HttpServer\Router\Router;
 
 Router::addRoute(['GET', 'POST', 'HEAD'], '/', 'App\Controller\IndexController::index');
 
-Router::addServer('ws', function(){
+Router::addServer('ws', function () {
     Router::get('/ws', App\Controller\WebSocketController::class);
 });
